56a3698c54d197f8e3690f0419e1ef35692b58fd
theodorepynos <100175555+theodorepynos@users.noreply.github.com>
2024-09-18 19:12:28 -0400

    Update thpyn.js
    Touring production of art

--- thpyn.js
+++ thpyn.js
@@ -1,3 +1,7 @@
+//hello, world
+
+//main main for the main() JSvein
+
 console.log = function(msg) {
   document.body.innerHTML += '<div style="background-color: #fff; padding: 10px; border: 1px solid #ccc;">' + msg + '</div>';
 }
