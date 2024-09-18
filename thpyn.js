c67d19f6c1c9736f1aa731ea66d14f3b80807d74
theodorepynos <100175555+theodorepynos@users.noreply.github.com>
2024-09-18 18:55:52 -0400

    Update thpyn.js
    Trial with js to page

--- thpyn.js
+++ thpyn.js
@@ -1,4 +1,3 @@
-function main() {
-    setInterval(function(){
-    location.reload();
-}, 1000;
+console.log = function(msg) {
+  document.body.innerHTML += '<div style="background-color: #fff; padding: 10px; border: 1px solid #ccc;">' + msg + '</div>';
+}
